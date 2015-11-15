9cf3a03db29ab3392a241f57e18bd86d14e20c5b
sagarzopate <sagar_zopate@rediffmail.com>
2015-11-15 21:20:12 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
-#include <stdarg.h>
+
+/*
+Copyright (C) <2015>  <Sagar Zopate>
+    This file is part of Text Editor.
+
+    Text Editor program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
